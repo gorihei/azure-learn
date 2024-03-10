31aa1b7fcd5b13147fa3d6307364da28368c0c77
gorihei <gorihei.ff@gmail.com>
2024-03-10 16:57:15 +0900

    add solution

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace azure_learn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

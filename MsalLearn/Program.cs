52dc25b33fff80cc106a2711412f3ec3e464ca65
gorihei <gorihei.ff@gmail.com>
2024-03-16 17:39:01 +0900

    20240316 az204msal

---
+++ MsalLearn/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Identity.Client;
+namespace MsalLearn
+{
+    public class Program
+    {
+        private const string _clientId = "9eaa2ff9-460c-4b17-bb0c-086288b178a1";
+        private const string _tenantId = "f73db128-b025-4ec6-b63a-c3b4eff7a04b";
+
+        static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            string[] scopes = { "user.read" };
+            var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
